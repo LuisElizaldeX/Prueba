169309814cd72ef3b24aea256b626c9dafcfae31
LuisElizaldeX <king_mbea@hotmail.com>
2024-05-11 16:08:58 -0600

    Creacion de metodo alerta simple

--- src/componentesjfx/Utilidades/Utils.java
+++ src/componentesjfx/Utilidades/Utils.java
@@ -1,9 +1,18 @@
 package componentesjfx.Utilidades;
 
+import javafx.scene.control.Alert;
+
 /**
  *
  * @author king_
  */
 public class Utils {
-    
+    public static void mostrarAlertaSimple(String titulo, String mensaje,
+        Alert.AlertType tipo) {
+        Alert alertaSimple = new Alert(tipo);
+        alertaSimple.setTitle(titulo);
+        alertaSimple.setContentText(mensaje);
+        alertaSimple.setHeaderText(null);
+        alertaSimple.showAndWait();
+    }
 }
